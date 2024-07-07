78519142be29a744b2b9eecf05c10f5294fa8172
Ansh Gupta <radixansh@gmail.com>
2024-07-08 00:47:28 +0530

    v1.1

--- pages/pricing.js
+++ pages/pricing.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
-//import styles from '../styles/Pricing.module.css'
+import styles from '../styles/Pricing.module.css'
 
 export default function Pricing() {
   return (
